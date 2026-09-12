d2dac27fd176f908c625f8d5fdcecfc963cb76e3
linhtruong309 <truongh@dickinson.edu>
2026-09-12 18:20:00 +0000

    complete the extension 1

--- web-projects/first-website/scripts/main.js
+++ web-projects/first-website/scripts/main.js
@@ -22,7 +22,7 @@ function setUserName() {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.textContent = `Mozilla is cool, ${myName}`;
+    myHeading.textContent = `Welcome to Vietnam, ${myName}`;
   }
 }
 
